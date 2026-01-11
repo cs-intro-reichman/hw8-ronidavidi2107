9930390d507e532835398a1f624578690b3aff68
ronidavidi2107 <roni.davidi@post.runi.ac.il>
2026-01-11 18:08:00 +0200

    final r

--- Network.java
+++ Network.java
@@ -57,7 +57,7 @@ public class Network {
     public boolean addFollowee(String name1, String name2) {
         User user1 = getUser(name1);
         User user2 = getUser(name2);
-        if (user1 == null || user2 == null || name1.equalsIgnoreCase(name2)) {
+        if (user1 == null || user2 == null || name1.equals(name2)) {
             return false;
         }
         return user1.addFollowee(name2);
